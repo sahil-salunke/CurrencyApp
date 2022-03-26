0636111493c401d9e008cd1669cc04e3d39bb0b9
sahilsalunke <sahil.salunke6194>
2022-03-26 21:11:18 +0530

    Design changes and code cleanup

--- app/src/main/java/com/example/currencyapp/presentation/views/HomeFragment.kt
+++ app/src/main/java/com/example/currencyapp/presentation/views/HomeFragment.kt
@@ -13,7 +13,6 @@ import androidx.navigation.fragment.findNavController
 import com.example.currencyapp.utils.EventListener
 import com.example.currencyapp.databinding.FragmentHomeBinding
 import com.example.currencyapp.presentation.viewmodel.HomeViewModel
-import com.example.currencyapp.utils.Utils.Companion.getConvertedCurrency
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -47,79 +46,6 @@ class HomeFragment : Fragment(), EventListener {
         binding.lifecycleOwner = this
 
         viewModel.getCurrencyData()
-        init()
-    }
-
-//    /**
-//     * Bind data to adapters
-//     */
-//    private fun setAdapter(symbols: LinkedHashMap<String, String>) {
-//        listOfCurrency = symbols.keys.toList() as ArrayList<String>
-//        val adapter = ArrayAdapter(
-//            requireContext(),
-//            R.layout.simple_spinner_dropdown_item,
-//            symbols.keys.toList() as ArrayList<String>
-//        )
-//        binding.spFrom.adapter = adapter
-//        binding.spTo.adapter = adapter
-//        binding.progress.visibility = View.GONE
-//    }
-
-    /**
-     * Initializations and events handling
-     */
-    private fun init() {
-
-//        binding.spFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(
-//                parent: AdapterView<*>?, view: View?, position: Int, id: Long
-//            ) {
-//                fromCurrency = listOfCurrency[position]
-//                fromCurrencyRate = rates?.get(fromCurrency)!!
-//            }
-//
-//            override fun onNothingSelected(parent: AdapterView<*>?) {}
-//        }
-//
-//        binding.spTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(
-//                parent: AdapterView<*>?, view: View?, position: Int, id: Long
-//            ) {
-//                toCurrency = listOfCurrency[position]
-//                toCurrencyRate = rates?.get(toCurrency)!!
-//            }
-//
-//            override fun onNothingSelected(parent: AdapterView<*>?) {
-//            }
-//        }
-
-//        binding.etAmountFrom.setOnEditorActionListener { _, i, _ ->
-//            if (i == EditorInfo.IME_ACTION_GO) {
-//                val fromValue = binding.etAmountFrom.text.toString().toDoubleOrNull()
-//                binding.etAmountTo.setText(
-//                    getConvertedCurrency(
-//                        fromCurrencyRate,
-//                        toCurrencyRate, fromValue
-//                    ).toString()
-//                )
-//            }
-//            false
-//        }
-
-//        binding.etAmountTo.setOnEditorActionListener { _, i, _ ->
-//            if (i == EditorInfo.IME_ACTION_GO) {
-//                val fromValue = binding.etAmountTo.text.toString().toDoubleOrNull()
-//                binding.etAmountFrom.setText(
-//                    getConvertedCurrency(
-//                        toCurrencyRate,
-//                        fromCurrencyRate, fromValue
-//                    ).toString()
-//                )
-//            }
-//            false
-//        }
-
-
     }
 
     override fun onGoButtonClick(view: View, actionId: Int, event: KeyEvent?): Boolean {
@@ -128,30 +54,14 @@ class HomeFragment : Fragment(), EventListener {
                 binding.etAmountFrom.id -> {
                     val fromValue = binding.etAmountFrom.text.toString().toDoubleOrNull()
                     viewModel.onToInputValue(fromValue)
-//                    binding.etAmountTo.setText(
-//                        getConvertedCurrency(
-//                            fromCurrencyRate,
-//                            toCurrencyRate, fromValue
-//                        ).toString()
-//                    )
                 }
                 binding.etAmountTo.id -> {
                     val fromValue = binding.etAmountTo.text.toString().toDoubleOrNull()
                     viewModel.onFromInputValue(fromValue)
-//                    binding.etAmountTo.setText(
-//                        getConvertedCurrency(
-//                            fromCurrencyRate,
-//                            toCurrencyRate, fromValue
-//                        ).toString()
-//                    )
                 }
             }
         }
         return true
-    }
-
-    override fun onButtonClick() {
-        findNavController().navigate(HomeFragmentDirections.actionHomeToDetails())
     }
 
     override fun onSelectItem(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
@@ -165,5 +75,8 @@ class HomeFragment : Fragment(), EventListener {
         }
     }
 
+    override fun onButtonClick() {
+        findNavController().navigate(HomeFragmentDirections.actionHomeToDetails())
+    }
 
 }
